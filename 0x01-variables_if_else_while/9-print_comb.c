fd5b4fee4020892e1c254a6a923cf4b8dd34531c
Okiemute <frankstar56@gmail.com>
2023-02-17 19:58:10 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,38 +1,26 @@
 #include <stdio.h>
 
+
 /**
-* main - entry point
+* main- prints all possible combinations of single-digit numbers
 *
-* Description: Prints all possible different combinations of two digits.
-* Numbers must be separated by ", " followed by a space
-* The two digits must be different
-* 01 and 10 are considered the same combination of the two digits 0 and 1
-* Print only the smallest combination of two digits
-* Numbers should be printed in ascending order, with two digits
-* You can only use the putchar function (every other function is forbidden)
-* You can only use putchar five times maximum in your code
-* You are not allowed to use any variable of type char
+* Return: Always 0.
 *
-* Return: Always 0 (success)
 */
+
 int main(void)
 {
-int i = 0;
+int num;
 
-while (i < 100)
+for (num = 0; num <= 9; num++)
 {
-if (i / 10 < i % 10)
-{
-putchar(i / 10 + '0');
-putchar(i % 10 + '0');
+putchar((num % 10) + '0');
+if (num == 9)
+continue;
 
-if (i < 89)
-{
 putchar(',');
 putchar(' ');
-}
-}
-i++;
+
 }
 
 putchar('\n');
