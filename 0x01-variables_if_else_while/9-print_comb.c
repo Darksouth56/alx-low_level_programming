d0e1d1c176d6d3d6927cc1de502ff1ce2faedbf2
Okiemute <frankstar56@gmail.com>
2023-02-17 20:38:55 -0800

    The success combination in business is: Do what you do better... and: do more of what you do.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,24 +1,40 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of single-digit numbers.
+ * main - entry point
  *
- * Return: Always 0.
+ * Description: Prints all possible different combinations of two digits.
+ * Numbers must be separated by ", " followed by a space
+ * The two digits must be different
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * Print only the smallest combination of two digits
+ * Numbers should be printed in ascending order, with two digits
+ * You can only use the putchar function (every other function is forbidden)
+ * You can only use putchar five times maximum in your code
+ * You are not allowed to use any variable of type char
+ *
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	int num;
+    int i = 0;
 
-	for (num = 0; num <= 9; num++)
-	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-		continue;
-		putchar(',');
-		putchar(' ');
-	}
+    while (i < 100)
+    {
+        if (i / 10 < i % 10)
+        {
+            putchar(i / 10 + '0');
+            putchar(i % 10 + '0');
 
-	putchar('\n');
+            if (i < 89)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+        i++;
+    }
 
-	return (0);
+    putchar('\n');
+    return (0);
 }
